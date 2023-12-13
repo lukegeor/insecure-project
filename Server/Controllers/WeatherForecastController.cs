4e99511edc3edea124a994f34c8d6b65046dfbe4
Luke Georgalas <lgeorgalas@illumina.com>
2023-12-13 15:59:37 -0800

    deleted comments and workflow

--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -8,21 +8,10 @@ namespace InsecureProject.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController(DbModelContext dbContext, ILogger<WeatherForecastController> logger) : ControllerBase
 {
-    // private static readonly string[] Summaries = {
-    //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    // };
-
     [HttpGet("{city}")]
     public IEnumerable<WeatherForecast> Get([FromRoute]string city)
     {
         logger.LogDebug($"Get called for city {city}.");
-        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        // {
-        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //     TemperatureC = Random.Shared.Next(-20, 55),
-        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        // })
-        // .ToArray();
 
         return dbContext.Forecasts.Where(f => f.City.ToLower().Equals(city.ToLower()))
             .Select(f => new WeatherForecast
