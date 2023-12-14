992d62e562ec5f8c80332ede8c7454d36ee708c7
Luke Georgalas <lgeorgalas@illumina.com>
2023-12-14 09:50:53 -0800

    Added direct reference to controller class.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,4 @@
+using InsecureProject.Controllers;
 using InsecureProject.Database;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<DbModelContext>();
+builder.Services.AddScoped<WeatherForecastController>();
 
 var app = builder.Build();
 
